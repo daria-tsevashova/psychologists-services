08c8ae1170c60b08f602b8b8364a711dad7f416c
Dari <dariatsevashova@gmail.com>
2026-05-06 23:59:39 +0200

    feat: setup core application pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
 export default function Home() {
   return (
     <main>
-      <h1>Psychologists Services</h1>
+      <div>
+        <h1>
+          The road to the <span>depths</span> of the human soul
+        </h1>
+        <p>
+          We help you to reveal your potential, overcome challenges and find a
+          guide in your own life with the help of our experienced psychologists.
+        </p>
+        <Link to="/psychologists" className={}>
+          Get started
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3 12L12 3M12 3H6M12 3V9"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Link>
+      </div>
+      <div>
+        <img src="#" alt="#" />
+        <div>
+          <span>Experienced psychologists</span>
+          <strong>15,000</strong>
+        </div>
+      </div>
     </main>
   );
 }
